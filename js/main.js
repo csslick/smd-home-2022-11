f8643e3fd15a18b10ff7bb97407202e06eaa8c34
csslick <digitone@naver.com>
2022-11-04 12:23:08 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,2 +1,20 @@
 // main.js
 console.log('main.js');
+
+$(function(){
+  // 배너 이미지 배열
+  let bgArr = [
+    'images/main-banner-e1.jpg',
+    'images/main-banner-e2.jpg',
+    'images/main-banner-e3.jpg',
+  ]
+
+  let bg_img_num = 0; // 이미지 번호
+
+  // 배경이미지 변경
+  $('#main-banner .container').css({
+    'background-image': `url(${bgArr[bg_img_num ]})`
+  })
+
+
+})
